3a11cc46a76983f849aa87edf3c12e2fff314b88
mannygarcia98 <mannysg88@gmail.com>
2022-04-22 15:51:06 -0500

    added PORT variable

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { notes } = require("./data/db.json");
 
 //Express
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, notesArray) {
@@ -21,6 +22,6 @@ app.get("/api/notes", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log("API server now on port 3001");
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
