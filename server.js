bdb7d982f6cfed2eecac2f68207c2c940294c83d
mannygarcia98 <mannysg88@gmail.com>
2022-04-22 15:28:19 -0500

    create /api/notes endpoint

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const { notes } = require("./data/db.json");
 const express = require("express");
 const app = express();
 
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.id) {
+    filteredResults = filteredResults.filter((note) => note.id === query.id);
+  }
+  return filteredResults;
+}
+
+app.get("/api/notes", (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
 app.listen(3001, () => {
   console.log("API server now on port 3001");
 });
