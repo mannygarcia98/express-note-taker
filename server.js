7d1b636c1340ec9fc1c746422c2aa5be3560614d
mannygarcia98 <mannysg88@gmail.com>
2022-04-22 16:31:08 -0500

    handle request for a specific note

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-function filterByQuery(query, notesArray) {
-  let filteredResults = notesArray;
-  if (query.id) {
-    filteredResults = filteredResults.filter((note) => note.id === query.id);
-  }
-  return filteredResults;
+function findById(id, notesArray) {
+  const result = notesArray.filter((note) => note.id === id)[0];
+  return result;
 }
 
 app.get("/api/notes", (req, res) => {
   let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
   res.json(results);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, () => {
