75a8638cee563f8e7721cf0b1fe8ce9363c20f85
mannygarcia98 <mannysg88@gmail.com>
2022-04-23 15:41:01 -0500

    add route to handle new note creation

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 //Note Data
 const { notes } = require("./data/db.json");
 
@@ -17,6 +20,23 @@ function findById(id, notesArray) {
   return result;
 }
 
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(path.join(__dirname, "./data/db.json"), JSON.stringify({ notes: notesArray }, null, 2));
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.name !== "string") {
+    return false;
+  }
+  if (!note.content || typeof note.content !== "string") {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/notes", (req, res) => {
   let results = notes;
   res.json(results);
@@ -32,9 +52,17 @@ app.get("/api/notes/:id", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-  //req.body is where our incoming content will be
-  console.log(req.body);
-  res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("Note is not properly formatted.");
+  } else {
+    //add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 app.listen(PORT, () => {
